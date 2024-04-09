2eda670cdde6a011f994364f5403b0f86bececb2
u225792 <marius.schaer2@sbb.ch>
2024-04-09 08:02:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This project is a minimal reproducer for
+[spring-cloud-stream/issues/2934](https://github.com/spring-cloud/spring-cloud-stream/issues/2934).
---
+++ src/main/java/dev/schaer/reproducer_2934/Reproducer2934Application.java
@@ -0,0 +1,13 @@
+package dev.schaer.reproducer_2934;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reproducer2934Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Reproducer2934Application.class, args);
+    }
+
+}
